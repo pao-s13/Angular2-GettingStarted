b6002dfe7fe99f3619667149fc42c152e56612dd
Paola Silvera <paola.silvera@incontact.com>
2017-03-30 17:50:28 -0400

    products list table and button functionality

---
+++ APM/app/products/product.ts
@@ -0,0 +1,27 @@
+export interface IProduct {
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    price: number;
+    description: string;
+    starRating: number;
+    imageUrl: string;
+}
+
+/*Example only */
+export class Product implements IProduct {
+    constructor(public productId: number,
+    public productName: string,
+    public releaseDate: string,
+    public price: number,
+    public description: string,
+    public startRating: number,
+    public imageUrl: string) {       
+    }
+
+    calculateDiscount(percent: number): number {
+        return this.price - (this.price * percent / 100);
+    }
+
+}
